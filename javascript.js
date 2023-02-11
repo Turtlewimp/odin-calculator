0aff8c99d24574426471c1bef5c26b9159328fbf
Turtlewimp <turtlewimp@gmail.com>
2023-02-10 19:32:55 -0800

    Mostly done with some bugs

--- javascript.js
+++ javascript.js
@@ -11,25 +11,155 @@ function multiply(a,b) {
 }
 
 function divide(a,b) {
-    return a/b;
+    if (b !== 0) return a/b;
 }
 
-function operate(operator,a,b) {
-    switch(operator) {
+function operate(calculation) {
+    a = Number(calculation.num1);
+    b = Number(calculation.num2);
+    switch(calculation.op) {
         case '+':
-            add(a,b);
-            break;
+            return add(a,b);
         case '-':
-            subtract(a,b);
-            break;
+            return subtract(a,b);
         case '*':
-            multiply(a,b);
-            break;
+            return multiply(a,b);
         case '/':
-            divide(a,b);
-            break;
+            return divide(a,b);
         default:
-            alert("operate() error")
-            break;
+            return alert("operate() error")
     }
 }
+
+
+function resetDisplay() {
+    const display = document.querySelector('#display');
+    let displayNum = '';
+    display.textContent = displayNum;
+}
+
+
+
+/*
+ <div class="keys">
+    <div data-key="65" class="key">
+      <kbd>A</kbd>
+      <span class="sound">clap</span>
+    </div>
+    <div data-key="83" class="key">
+      <kbd>S</kbd>
+      <span class="sound">hihat</span>
+    </div>
+    <div data-key="68" class="key">
+      <kbd>D</kbd>
+      <span class="sound">kick</span>
+    </div>
+    <div data-key="70" class="key">
+      <kbd>F</kbd>
+      <span class="sound">openhat</span>
+    </div>
+    <div data-key="71" class="key">
+      <kbd>G</kbd>
+      <span class="sound">boom</span>
+    </div>
+    <div data-key="72" class="key">
+      <kbd>H</kbd>
+      <span class="sound">ride</span>
+    </div>
+    <div data-key="74" class="key">
+      <kbd>J</kbd>
+      <span class="sound">snare</span>
+    </div>
+    <div data-key="75" class="key">
+      <kbd>K</kbd>
+      <span class="sound">tom</span>
+    </div>
+    <div data-key="76" class="key">
+      <kbd>L</kbd>
+      <span class="sound">tink</span>
+    </div>
+  </div>
+
+ function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
+
+  
+*/
+
+/*
+const display = document.querySelector('#display');
+    let displayNum = '0';
+    display.textContent = displayNum;
+    //let calculation = {};
+
+    //if button press, add button id to display string of numbers
+    const numbers = document.querySelectorAll('.number');
+    numbers.forEach((number) => {
+        number.addEventListener('click', () => {
+            
+            //displayNum += number.id;
+            display.textContent = number.id;
+
+            //if empty
+            //add any number except zeor
+            //if not empty
+            //add any number
+            
+            if(displayNum === '') {
+                if(number.id>0) {
+                    displayNum += number.id;
+                    display.textContent = displayNum;
+                }
+            } else {
+                displayNum += number.id;
+                display.textContent = displayNum;
+            }
+
+        });
+    });
+
+    
+    const operators = document.querySelectorAll('.operator');
+    operators.forEach((operator) => {
+        operator.addEventListener('click', () => {
+            //if first time click
+            //first check if there is number
+            if(displayNum) {
+
+            }
+            displayNum += displayElement.id;
+            display.textContent = displayNum;
+
+            
+
+            console.log(calculation);
+            calculation.push(displayElement.id);
+            console.log(calculation);
+        });
+    });
+    
+
+//if first time operator click
+
+
+    
+    const equals = document.querySelector('#equals');
+    //equals.addEventListener('click', operate(calculation));
+    
+    //if clear button press, replace display string to empty string*/
